686776a9a13709c977e18893d1af40dc17ce8a9e
Anish Bhanushali <111268352+Anishbhanushali@users.noreply.github.com>
2023-09-18 17:46:51 +0530

    Jordans-js.js

--- Jordans-js.js
+++ Jordans-js.js
@@ -1,1 +1,5 @@
-
+function changing(option){
+            let content = document.querySelector("#body");
+            body.style.background = option;
+            document.querySelector('#mainimg').src = option;
+        }
